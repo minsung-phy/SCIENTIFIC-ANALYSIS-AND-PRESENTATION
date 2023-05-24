e863c6681000dfdc8475ba659748dc5254de4151
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-05-24 21:51:35 +0900

    Add files via upload

--- 지하철 호선별 역별 승하차 인원 정보/지하철 호선별 승하차 인원 정보 2020.py
+++ 지하철 호선별 역별 승하차 인원 정보/지하철 호선별 승하차 인원 정보 2020.py
@@ -6,7 +6,7 @@ from matplotlib import rc
 rc('font', family='AppleGothic') 		
 plt.rcParams['axes.unicode_minus'] = False 
 
-data = pd.read_csv("/Users/minsung/Documents/2학년 1학기/과학적분석및발표/IC-PBL/서울시 지하철호선별 역별 승하차 인원 정보 2020.csv", encoding='cp949')
+data = pd.read_csv("/Users/minsung/Documents/2학년 1학기/과학적분석및발표/IC-PBL/지하철 호선별 역별 승하차 인원 정보/서울시 지하철호선별 역별 승하차 인원 정보 2020.csv", encoding='cp949')
 
 data = data[['노선명', '승차총승객수', '하차총승객수']]
 data['총승객수'] = data['승차총승객수'] + data['하차총승객수']
@@ -16,8 +16,9 @@ grouped_data = data.groupby('노선명')['총승객수'].sum().reset_index()
 x = grouped_data['노선명']
 y = grouped_data['총승객수']
 
+plt.title("2020 호선별 승하차 인원 정보")
 plt.bar(x, y)
 plt.xticks(rotation=90)
 plt.xlabel('노선명')
-plt.ylabel('승차총승객수+하차총승객수')
+plt.ylabel('총승객수')
 plt.show()
